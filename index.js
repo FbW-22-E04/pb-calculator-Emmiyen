a23dd7c11dd857cb44c71c86fa83051e402f3bb3
Danica Yener <ny21037@googlemail.com>
2022-09-05 17:27:10 +0200

    calulator

--- index.js
+++ index.js
@@ -1,10 +1,74 @@
-
 class Calculator{
     constructor(){
-        //properties
+        this.PI = Math.PI;
+        this.E = Math.exp(1)
     }
 
-    //getter
+    //Getter
+    get pi(){
+        return `PI (${this.PI})`
+    }
+
+
+    get e(){
+        return `Eulner's number (${this.E})`
+    }
+
+    ratio(x,y,width){
+        let widthRatio=width/x;
+        let heightRatio=widthRatio*y;
+        return `height is ${heightRatio} on ratio ${x}:${y}`
+    } 
+
+   
+    percentage(x,y) {
+        return `${(x/y)*100}%`
+    }
+
+    add(x, y){
+        return x+y
+    } 
+
+  
+    subtract(x, y){
+        return y-x
+    } 
+
+  
+    multiply(x, y) {
+        return x*y
+    }
+
     
-    //methods
+    divide(x, y) {
+        if(y===0){
+            return "Error: Divisor can't be 0"
+        }else{
+            return x/y
+        }
+
+    }
+
+    modulation(x, y){
+        if(y===0){
+            return "Error: Divisor can't be 0"
+        }else{
+            return (x%y)
+        }
+    }  
+
+ 
+    elevate(x, y) {
+        return Math.pow(x,y)
+    }
+
+
+    sqrt(x) {
+        return Math.sqrt(x)
+    }
+
 }
+
+
+const calculator= new Calculator()
+console.log(calculator.ratio(3,4,600))
